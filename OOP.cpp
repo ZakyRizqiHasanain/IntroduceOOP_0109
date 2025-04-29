36568285710706fbd989d1a8119d7c1303b050fe
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-04-29 18:23:12 +0700

    Membuat class mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -2,22 +2,9 @@
 using namespace std;
 
 class Mahasiswa{
-    public :
-    int nim;
-    string nama;
-    float nilai;
-    void printdata(){
-        cout << "NIM : " << nim << endl;
-        cout << "NAMA : " << nama << endl;
-        cout << "NILAI : " << nilai << endl;
-    }
+    
 };
 
-int main (){
-    Mahasiswa mhs;
-    mhs.nim = 109;
-    mhs.nama = "Zaky";
-    mhs.nilai = 87;
-
-    mhs.printdata();
+int main(){
+    
 }
